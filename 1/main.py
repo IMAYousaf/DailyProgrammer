d1bf6ae24b71a2120557eceef577a36722914980
IMAYousaf <IMAYousaf@gmail.com>
2016-12-27 15:10:54 -0800

    Challenge 1 - Easy
    Partially complete. Will revisit later.

---
+++ 1/main.py
@@ -0,0 +1,19 @@
+'''
+create a program that will ask the users name, age, and reddit username. have it tell them the information back, in the format:
+your name is (blank), you are (blank) years old, and your username is (blank)
+for extra credit, have the program log this information in a file to be accessed later.
+'''
+
+def main():
+    name = input("Please input your name:")
+    age = input("Please input your age:")
+    username = input("Please input your username:")
+
+    file = open("information.txt", "w")
+    file.write(name)
+    file.write(age)
+    file.write(username)
+
+    print ("Your name is " + name + ", you are " + age + " years old, and your username is " + username + ".")
+
+main()
